04d70bcf2a4747f7d97b07838d66322e50f39cbe
Colibecas <Colibecas@HP255G8>
2024-10-21 18:59:58 -0600

    Agregar archivos de proyecto.

---
+++ Products/Products/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Products.Models
+{
+    public class Products
+    {
+        [Required(ErrorMessage = "El Inventario es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El Inventario debe ser mayor a 0")]
+        public int? Inventario { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Departamento es obligatorio")]
+        public string? Departamento { get; set; }
+    }
+}
